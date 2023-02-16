000f36380b724756037b02eda9d33f726d6112e8
PreciousBraimah <preciousb543@gmail.com>
2023-02-16 15:15:16 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <unlstd.h>
+#include <unistd.h>
+
 /**
- * main -A c program that prints a line to the standard error
- * Return: 1 (Success)
+ * main - A c program that prints a line to the standard error
+ * Return: Always 1.
  */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59i);
 return (1);
 }
