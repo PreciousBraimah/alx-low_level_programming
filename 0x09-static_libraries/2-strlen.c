47772b0c1b3b48ad05c03f201ec629629e0d88a2
PreciousBraimah <preciousb543@gmail.com>
2023-03-13 03:28:16 -0700

    Updated

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,10 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
-int _putchar(char c)
+int _strlen(char *s)
 {
-	return putchar(c);
+    return strlen(s);
 }
